79dcdcdd5b520a5bb7a93b9ec45c4a65adcfef0a
Venkateswarlu Allacheruvu <vallacheruvu@gmail.com>
2023-06-01 16:31:35 +0530

    Contact info added

--- src/pages/contact.py
+++ src/pages/contact.py
@@ -6,8 +6,40 @@ import dash_bootstrap_components as dbc
 
 dash.register_page(__name__, path = "/contact", name = "Contact", order = 6)
 
+contact_content_mobile = [
+    dbc.CardHeader("Mobile"),
+    dbc.CardBody(
+        [
+            html.H5("+919966090437"),
+            html.P(
+                "India",
+                className="card-text",
+            ),
+            html.P(
+                "Available from 09:30 to 1830 IST everyday"
+            )
+        ]
+    ),
+]
+contact_content_email = [
+    dbc.CardHeader("Email"),
+    dbc.CardBody(
+        [
+            html.H5("vallacheruvu@gmail.com")
+        ]
+    ),
+]
+
 layout = dbc.Container(
     [
-        html.H1("hello")
+        dbc.Row(
+            [
+                dbc.Col(dbc.Card(contact_content_mobile, color="primary", outline=True)),
+                dbc.Col(dbc.Card(contact_content_email, color="primary", outline=True))
+            ],
+            className="mb-4",
+        )
     ]
 )
+
+
