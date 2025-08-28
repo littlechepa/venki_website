d5cf1299cbd77732c248341bcaf566b661da990e
Venkateswarlu Allacheruvu <vallacheruvu@gmail.com>
2025-08-29 00:21:59 +0530

    cert rendering fixed

--- src/pages/certifications.py
+++ src/pages/certifications.py
@@ -18,7 +18,7 @@ layout = dbc.Container(
     Input("div-cert", "children")
 )
 def render_certs(value):
-    folder_path = os.path.join(os.getcwd(), "assets/certificates")
+    folder_path = os.path.join(os.getcwd(), "src/assets/certificates")
     cert_children = []
     for file_name in os.listdir(folder_path):
         cert_children.append(html.Img(src = f"assets/certificates/{file_name}", className = "cert-img"))
