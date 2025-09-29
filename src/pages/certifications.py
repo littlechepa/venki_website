df89e090b75306386fd83d7bdfb48733279501dd
Venkateswarlu Allacheruvu <vallacheruvu@gmail.com>
2025-09-30 00:38:10 +0530

    code change

--- src/pages/certifications.py
+++ src/pages/certifications.py
@@ -20,6 +20,5 @@ layout = dbc.Container(
 def render_certs(value):
     folder_path = os.path.join(os.getcwd(), "src/assets/certificates")
     cert_children = []
-    for file_name in os.listdir(folder_path):
-        cert_children.append(html.Img(src = f"assets/certificates/{file_name}", className = "cert-img"))
+
     return cert_children
